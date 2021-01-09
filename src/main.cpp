3dc4eac5c82737378d7c450b1a8571f952293ae7
David Misiak <misiakdavid@gmail.com>
2021-01-09 22:35:11 +0100

    Add helper functions for terminal colors

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,11 @@
 int main(int argc, char **argv) {
     CLI::App app{"Tiler - tool for automated solving of polyomino tiling problems\n"};
 
-    std::atexit([]() {
-        std::cout << rang::style::reset;
-        std::cerr << rang::style::reset;
-    });
+    std::atexit([]() { print::normal(); });
     try {
         app.parse(argc, argv);
     } catch (const CLI::ParseError &e) {
-        if (e.get_exit_code() == 0) {
-            std::cout << rang::fg::blue;
-        } else {
-            std::cerr << rang::fg::red;
-        }
+        e.get_exit_code() == 0 ? print::help() : print::error();
         return app.exit(e);
     }
     return 0;
