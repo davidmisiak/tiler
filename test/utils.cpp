401bcf811338e73d50433488807bc6e71d5d631f
David Misiak <misiakdavid@gmail.com>
2021-03-19 16:05:06 +0100

    Add remove_margins test

--- test/utils.cpp
+++ test/utils.cpp
@@ -2,6 +2,25 @@
 
 #include "catch2/catch.hpp"
 
+TEST_CASE("Margins are trimmed") {
+    using Result = std::tuple<int, int, utils::BoolMatrix>;
+    REQUIRE(utils::remove_margins(0, 0, {}) == Result{0, 0, {}});
+    REQUIRE(utils::remove_margins(0, 1, {{}}) == Result{0, 0, {}});
+    REQUIRE(utils::remove_margins(1, 0, {}) == Result{0, 0, {}});
+    REQUIRE(utils::remove_margins(1, 1, {{0}}) == Result{0, 0, {}});
+    REQUIRE(utils::remove_margins(1, 1, {{1}}) == Result{1, 1, {{1}}});
+    REQUIRE(utils::remove_margins(1, 3, {{0}, {0}, {0}}) == Result{0, 0, {}});
+    REQUIRE(utils::remove_margins(2, 1, {{0, 1}}) == Result{1, 1, {{1}}});
+    REQUIRE(utils::remove_margins(2, 2, {{0, 1}, {1, 0}}) == Result{2, 2, {{0, 1}, {1, 0}}});
+    REQUIRE(utils::remove_margins(2, 2, {{0, 1}, {0, 0}}) == Result{1, 1, {{1}}});
+    REQUIRE(utils::remove_margins(5, 5,
+                                  {{0, 0, 0, 0, 0},
+                                   {0, 0, 0, 0, 0},
+                                   {0, 1, 1, 0, 0},
+                                   {0, 0, 1, 0, 0},
+                                   {0, 0, 0, 0, 0}}) == Result{2, 2, {{1, 1}, {0, 1}}});
+}
+
 TEST_CASE("Continuity check works") {
     REQUIRE(utils::is_continuous(0, 0, {}));
     REQUIRE(utils::is_continuous(0, 1, {{}}));
