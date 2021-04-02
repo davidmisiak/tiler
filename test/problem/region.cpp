78bb3322d9ff2e3af4a732f991a73a2d0ae33b09
David Misiak <misiakdavid@gmail.com>
2021-04-02 13:55:16 +0200

    Add more region parsing tests

--- test/problem/region.cpp
+++ test/problem/region.cpp
@@ -26,3 +26,41 @@ TEST_CASE("Regions defined by map are parsed") {
     REQUIRE(Region::parse("x\nx\nx") == Region(1, 3, {{1}, {1}, {1}}));
     REQUIRE(Region::parse(" xx\nxx") == Region(3, 2, {{0, 1, 1}, {1, 1, 0}}));
 }
+
+TEST_CASE("Region extra-whitespace trimming is performed") {
+    REQUIRE(Region::parse("  xx\n xx") == Region(3, 2, {{0, 1, 1}, {1, 1, 0}}));
+    REQUIRE(Region::parse_raw("  xx\n xx") == Region(4, 2, {{0, 0, 1, 1}, {0, 1, 1, 0}}));
+
+    REQUIRE(Region::parse("  \n x \n ") == Region(1, 1, {{1}}));
+    REQUIRE(Region::parse_raw("  \n x \n ") == Region(3, 3, {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
+
+    REQUIRE(Region::parse(" \n \n x \n \n ") == Region(1, 1, {{1}}));
+    REQUIRE(Region::parse_raw(" \n \n x \n \n ") ==
+            Region(3, 5, {{0, 0, 0}, {0, 0, 0}, {0, 1, 0}, {0, 0, 0}, {0, 0, 0}}));
+}
+
+TEST_CASE("Continuity and holes in regions are handled") {
+    REQUIRE_THROWS_AS(Region::parse("x\n x"), ParseError);
+    REQUIRE_NOTHROW(Region::parse_raw("x\n x"));
+
+    REQUIRE_THROWS_AS(Region::parse("xx x\nx  x\n xxx"), ParseError);
+    REQUIRE_NOTHROW(Region::parse_raw("xx x\nx  x\n xxx"));
+
+    REQUIRE_THROWS_AS(Region::parse("xxx\nx x\nxxx"), ParseError);
+    REQUIRE_NOTHROW(Region::parse_raw("xxx\nx x\nxxx"));
+
+    REQUIRE_THROWS_AS(Region::parse("xxxxx\nx   x\nx x x\nx   x\nxxxxx"), ParseError);
+    REQUIRE_NOTHROW(Region::parse_raw("xxxxx\nx   x\nx x x\nx   x\nxxxxx"));
+}
+
+TEST_CASE("Incorrectly defined regions are not parsed") {
+    REQUIRE_THROWS_AS(Region::parse(""), ParseError);
+    REQUIRE_THROWS_AS(Region::parse(" "), ParseError);
+    REQUIRE_THROWS_AS(Region::parse("\n"), ParseError);
+    REQUIRE_THROWS_AS(Region::parse("3"), ParseError);
+    REQUIRE_THROWS_AS(Region::parse("3l"), ParseError);
+    REQUIRE_THROWS_AS(Region::parse("y"), ParseError);
+    REQUIRE_THROWS_AS(Region::parse("1xx"), ParseError);
+    REQUIRE_THROWS_AS(Region::parse("5x0"), ParseError);
+    REQUIRE_THROWS_AS(Region::parse("x\n\nx"), ParseError);
+}
