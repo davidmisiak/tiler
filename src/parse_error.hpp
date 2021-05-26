5416b6d72ac1cc8094bacc69462b69be181e7094
David Misiak <misiakdavid@gmail.com>
2021-05-26 23:27:50 +0200

    Fix typo

--- src/parse_error.hpp
+++ src/parse_error.hpp
@@ -1,5 +1,5 @@
-#ifndef TILER_PROBLEM_PARSE_ERROR_HPP_
-#define TILER_PROBLEM_PARSE_ERROR_HPP_
+#ifndef TILER_PARSE_ERROR_HPP_
+#define TILER_PARSE_ERROR_HPP_
 
 #include <stdexcept>
 #include <string>
@@ -9,4 +9,4 @@ public:
     explicit ParseError(const std::string& err) : std::runtime_error(err){};
 };
 
-#endif  // TILER_PROBLEM_PARSE_ERROR_HPP_
+#endif  // TILER_PARSE_ERROR_HPP_
