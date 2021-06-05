684f898b1eac87c9f41fe905a0159c2041c05538
David Misiak <misiakdavid@gmail.com>
2021-06-05 17:04:53 +0200

    Minor comment fix

--- src/solution/solution.hpp
+++ src/solution/solution.hpp
@@ -9,7 +9,7 @@
 #include "solution/placed_region.hpp"
 
 // Contains the regions used in the solution together with their coordinates. The regions' order
-// can be arbitrary.
+// can be arbitrary. If there are no regions, the solution is considered non-existent.
 class Solution : public std::vector<PlacedRegion> {
 public:
     void save_image(std::string filepath, Problem problem);
@@ -19,7 +19,7 @@ private:
     // Size of a cell in pixels (does not matter much, because the generated image is SVG).
     static const int kCellSize;
 
-    // Colors of named tiles (in their lexicograpical order).
+    // Colors of named tiles (in their lexicographical order).
     static const std::vector<std::string> kColors;
 };
 
