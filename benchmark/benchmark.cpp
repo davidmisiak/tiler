26b0da6f881bf683a70d978bcf1853e886ea4023
David Misiak <misiakdavid@gmail.com>
2021-06-09 20:13:53 +0200

    Implement benchmarking function

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -1,13 +1,28 @@
 #include "benchmark/benchmark.h"
 
-static void benchmark_solve(benchmark::State& state) {
-    // Perform setup here
+#include <iostream>
+
+#include "parsers/problem_parser.hpp"
+#include "problem/problem.hpp"
+#include "solvers/solver_factory.hpp"
+
+auto solve = [](benchmark::State& state, Problem problem, std::string solver_name) {
     for (auto _ : state) {
-        // This code gets timed
-        int i = 1 + 1;
+        solver_factory::create(solver_name, problem)->solve();
     }
+};
+
+int main(int argc, char** argv) {
+    std::string filepath;
+    while (std::cin >> filepath) {
+        Problem problem = problem_parser::parse_from_file(filepath, false);
+        for (auto solver_name : solver_factory::solver_names) {
+            std::string name = filepath + "@" + solver_name;
+            benchmark::RegisterBenchmark(name.c_str(), solve, problem, solver_name)
+                    ->Unit(benchmark::kMillisecond);
+        }
+    }
+    benchmark::Initialize(&argc, argv);
+    if (benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+    benchmark::RunSpecifiedBenchmarks();
 }
-
-BENCHMARK(benchmark_solve);
-
-BENCHMARK_MAIN();
