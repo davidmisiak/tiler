2e6cef1985c79dea4f0796e9ed42dfeb3d2c6393
David Misiak <misiakdavid@gmail.com>
2022-03-21 21:09:19 +0100

    Add benchmark ignore list

--- scripts/release-benchmark.sh
+++ scripts/release-benchmark.sh
@@ -5,4 +5,5 @@ source scripts/gurobi-env.sh
 /bin/time -v ./release/bin/tilerbm \
     --benchmark_out="benchmark/$(date '+%Y-%m-%d_%H-%M-%S').json" \
     --benchmark_filter='' \
+    # --benchmark_filter="-$(cat benchmark/known-timeout.txt | head -c -1 | tr '\n' '|')" \
     # --benchmark_repetitions=10 --benchmark_report_aggregates_only=true
