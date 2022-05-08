b2bf58a75a2a95480770871d0ed02def6aee84a3
David Misiak <misiakdavid@gmail.com>
2022-05-08 21:34:38 +0200

    Remove aggregation options from benchmark script

--- scripts/release-benchmark.sh
+++ scripts/release-benchmark.sh
@@ -5,5 +5,4 @@ source scripts/load-env.sh
 /bin/time -v ./release/bin/tilerbm \
     --benchmark_out="benchmark/$(date '+%Y-%m-%d_%H-%M-%S').json" \
     --benchmark_filter='' \
-    # --benchmark_filter="-$(cat benchmark/known-timeout.txt | head -c -1 | tr '\n' '|')" \
-    # --benchmark_repetitions=10 --benchmark_report_aggregates_only=true
+    # --benchmark_filter="-$(cat benchmark/known-timeout.txt | head -c -1 | tr '\n' '|')"
