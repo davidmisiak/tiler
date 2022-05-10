789eb94b4151a52eba9d7655b0fc87ec103f7ecc
David Misiak <misiakdavid@gmail.com>
2022-05-10 23:34:18 +0200

    Add default benchmark filter

--- scripts/release-benchmark.sh
+++ scripts/release-benchmark.sh
@@ -4,5 +4,5 @@ source scripts/load-env.sh
 
 /bin/time -v ./release/bin/tilerbm \
     --benchmark_out="benchmark/$(date '+%Y-%m-%d_%H-%M-%S').json" \
-    --benchmark_filter='' \
+    --benchmark_filter='@simple|@dlx|@sat|@ilp' \
     # --benchmark_filter="-$(cat benchmark/known-timeout.txt | head -c -1 | tr '\n' '|')"
